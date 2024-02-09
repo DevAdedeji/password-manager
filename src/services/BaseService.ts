b9fa9d5135d47ca99540b96d19cbd2dca1e1e3f0
DevAdedeji <adedejitewogbade2@gmail.com>
2024-02-09 20:22:23 +0100

    Done

--- src/services/BaseService.ts
+++ src/services/BaseService.ts
@@ -1,5 +1,5 @@
 import axios, { type AxiosInstance, type AxiosRequestConfig } from "axios"
-import { useStorage } from "@/composables/storage";
+
 export class BaseApiService {
     private readonly BASE_URL = import.meta.env.VITE_APP_API_BASE_URL
     public axiosInstance: AxiosInstance
@@ -13,17 +13,6 @@ export class BaseApiService {
             baseURL: this.BASE_URL
         }
         this.axiosInstance = axios.create(this.config)
-        this.init()
-    }
-    private async init() {
-        const { getItem } = useStorage()
-        const token = await getItem("auth.token")
-        if(token){
-            this.setAuthorizationHeader(token)
-        }
-    }
-    private async setAuthorizationHeader(token: string) {
-        this.axiosInstance.defaults.headers.common['Authorization'] = `${token}`
     }
     public getUrl(){
         return this.resource
--- src/services/password/index.ts
+++ src/services/password/index.ts
@@ -1,24 +1,39 @@
 import { type AxiosResponse } from "axios";
 import { BaseApiService } from "../BaseService";
 import { type PasswordModel } from "./types";
+import { useStorage } from "@/composables/storage";
 
 export class PasswordService extends BaseApiService {
     constructor(){
         super('/password/')
     }
+    private async setAuthorizationHeader(token: string) {
+        this.axiosInstance.defaults.headers.common['Authorization'] = `${token}`
+    }
+    private async init() {
+        const { getItem } = useStorage()
+        const token = await getItem("auth.token")
+        if(token){
+            this.setAuthorizationHeader(token)
+        }
+    }
     public async fetchPasswords (): Promise<AxiosResponse> {
+        await this.init()
         const response: AxiosResponse = await this.axiosInstance.get('/user/me/details')
         return response
     }
     public async addPassword (data: PasswordModel): Promise<AxiosResponse> {
+        await this.init()
         const response: AxiosResponse = await this.axiosInstance.post(`${this.getUrl()}`, data)
         return response
     }
     public async deletePassword (id: string): Promise<AxiosResponse> {
+        await this.init()
         const response: AxiosResponse = await this.axiosInstance.delete(`${this.getUrl()}${id}`)
         return response
     }
     public async editPassword (id: string, data: PasswordModel): Promise<AxiosResponse> {
+        await this.init()
         const response: AxiosResponse = await this.axiosInstance.put(`${this.getUrl()}${id}`, data)
         return response
     }
